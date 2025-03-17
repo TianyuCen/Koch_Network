130b487490e2769b1ae10babebae9b9e261036d0
CrasherChris <CrasherChris@github.com>
2025-03-17 16:36:43 +0800

    It is now possible to build fractal networks larger than 3 dimensions.

--- main.py
+++ main.py
@@ -11,26 +11,25 @@ class Network:
         self.node = np.zeros((self.dimension, 48), dtype=bool)   # 48bit address
         self.network = {}
         self.segmentNum = 0
-        self.segCache = np.array([])      # Initialize segment
+        self.segCache = np.array([[]])      # Initialize segment
 
     def buildNetwork(self):
         timeStart = time.time()
-        self.node[0][:6], self.node[1][:6], self.node[2][:6], self.node[3][:6] = self.writeAddr(0)
+        self.node[:, :6] = self.writeAddr(0)
         self.network['1'] = self.node.copy()       # First Epoch
         self.updateSeg(0)
-        print("Iteration 1:\nSegment Num:4\nNode Num:4")
+        print("Iteration 1:\nSegment Num:%d\nNode Num:%d" % (self.dimension, self.dimension))
 
         i = 2
         iter = 1
         while True:
             for seg in self.segCache:       # Generate new nodes on existing segments
-                (self.node[0][:6 * (iter + 1)], self.node[1][:6 * (iter + 1)], self.node[2][:6 * (iter + 1)],
-                     self.node[3][:6 * (iter + 1)]) = self.writeAddr(iter, segment=seg)
+                self.node[:, :6 * (iter + 1)] = self.writeAddr(iter, segment=seg)
                 self.network[str(seg)] = self.node.copy()
                 i += 1
             self.updateSeg(iter)
             print("Iteration " + str(iter + 1) + ":\nSegment Num:%d\nNode Num:%d" %
-                  (len(self.segCache), len(self.network) * 4))
+                  (len(self.segCache), len(self.network) * self.dimension))
             if iter == self.iteration:
                 self.segmentNum = i - 1
                 break
@@ -40,79 +39,48 @@ class Network:
         timeUsage = timeEnd - timeStart
         print("Network has been created.Time Consumed: %.2fs" % timeUsage)
         print('Segment Num:', len(self.segCache))
-        print('Node Num:', len(self.network) * 4)
+        print('Node Num:', len(self.network) * self.dimension)
+        # print(self.network)
 
     def writeAddr(self, iter, segment=[]):
-        newAddr1 = np.array([], dtype=bool)
-        newAddr2 = np.array([], dtype=bool)
-        newAddr3 = np.array([], dtype=bool)
-        newAddr4 = np.array([], dtype=bool)
-
+        newAddrs = [ [] for _ in range(self.dimension) ]
+        newNodes = np.empty((self.dimension, self.segCache.shape[1]+2), dtype=int)
         if iter == 0:
-            newNode1 = np.append(self.segCache, [1, 0])       # Create new node on given segment
-            newNode2 = np.append(self.segCache, [2, 0])
-            newNode3 = np.append(self.segCache, [3, 0])
-            newNode4 = np.append(self.segCache, [4, 0])
-        else:
-            newNode1 = np.append(segment, [1, 0])  # Create new node on given segment
-            newNode2 = np.append(segment, [2, 0])
-            newNode3 = np.append(segment, [3, 0])
-            newNode4 = np.append(segment, [4, 0])
+            for i in range(self.dimension): # Create new node on given segment
+                newNodes[i] = np.append(self.segCache, [i + 1, 0])
+        else:       # Create new node on given segment
+            for i in range(self.dimension):
+                newNodes[i] = np.append(segment, [i + 1, 0])
 
-        for i in range(0, len(newNode1)):
-            addr = bin(int(newNode1[i]))[2:]                                           # Convert subscript to bin string
-            a_temp = np.array([int(j) for j in addr], dtype=bool)       # Convert bin string to bool array
-            a_temp = zeroSupplement(a_temp)
-            newAddr1 = np.append(newAddr1, a_temp)
+        for j in range(self.dimension):
+            for i in range(len(newNodes[j])):
+                addr = bin(int(newNodes[j][i]))[2:]
+                a_temp = np.array([int(k) for k in addr], dtype=bool)  # Convert bin string to bool array
+                a_temp = zeroSupplement(a_temp)
+                newAddrs[j].append(a_temp.copy())
 
-        for i in range(0, len(newNode2)):
-            addr = bin(int(newNode2[i]))[2:]                                           # Convert subscript to bin string
-            a_temp = np.array([int(j) for j in addr], dtype=bool)       # Convert bin string to bool array
-            a_temp = zeroSupplement(a_temp)
-            newAddr2 = np.append(newAddr2, a_temp)
-
-        for i in range(0, len(newNode3)):
-            addr = bin(int(newNode3[i]))[2:]                                           # Convert subscript to bin string
-            a_temp = np.array([int(j) for j in addr], dtype=bool)       # Convert bin string to bool array
-            a_temp = zeroSupplement(a_temp)
-            newAddr3 = np.append(newAddr3, a_temp)
-
-        for i in range(0, len(newNode4)):
-            addr = bin(int(newNode4[i]))[2:]                                           # Convert subscript to bin string
-            a_temp = np.array([int(j) for j in addr], dtype=bool)       # Convert bin string to bool array
-            a_temp = zeroSupplement(a_temp)
-            newAddr4 = np.append(newAddr4, a_temp)
-
-        return newAddr1, newAddr2, newAddr3, newAddr4
+        return np.array(newAddrs).reshape(self.dimension, 6 + 6*iter).copy()
 
     def updateSeg(self, iter):
         if iter == 0:
-            self.segCache = np.array([[1, 0], [2, 0], [3, 0], [4, 0]])
+            self.segCache = np.zeros((self.dimension, 2), dtype=int)
+            for i in range(self.dimension):
+                self.segCache[i][0] = i + 1
             return
         elif iter == 1:
             segmentTemp = np.array([], dtype=int)      # Segment update of first iteration(subscript0, 1)
             segmentTemp = np.append(segmentTemp, np.append(self.segCache[0], [1, 0]))
             for segment in self.segCache:
-                segmentTemp = np.vstack([segmentTemp, np.append(segment, [1, 0])])
-                segmentTemp = np.vstack([segmentTemp, np.append(segment, [2, 0])])
-                segmentTemp = np.vstack([segmentTemp, np.append(segment, [3, 0])])
-                segmentTemp = np.vstack([segmentTemp, np.append(segment, [4, 0])])
-                segmentTemp = np.vstack([segmentTemp, np.append(segment, [1, 1])])
-                segmentTemp = np.vstack([segmentTemp, np.append(segment, [2, 1])])
-                segmentTemp = np.vstack([segmentTemp, np.append(segment, [3, 1])])
-                segmentTemp = np.vstack([segmentTemp, np.append(segment, [4, 1])])
+                for dim in range(self.dimension):
+                    segmentTemp = np.vstack([segmentTemp, np.append(segment, [dim+1, 0])])
+                    segmentTemp = np.vstack([segmentTemp, np.append(segment, [dim+1, 1])])
         else:                               # Segment of thr rest of iterations(subscript1, 2)
             segmentTemp = np.array([], dtype=int)
             segmentTemp = np.append(segmentTemp, np.append(self.segCache[0], [1, 0]))
             for segment in self.segCache:
-                segmentTemp = np.vstack([segmentTemp, np.append(segment, [1, 1])])
-                segmentTemp = np.vstack([segmentTemp, np.append(segment, [2, 1])])
-                segmentTemp = np.vstack([segmentTemp, np.append(segment, [3, 1])])
-                segmentTemp = np.vstack([segmentTemp, np.append(segment, [4, 1])])
-                segmentTemp = np.vstack([segmentTemp, np.append(segment, [1, 2])])
-                segmentTemp = np.vstack([segmentTemp, np.append(segment, [2, 2])])
-                segmentTemp = np.vstack([segmentTemp, np.append(segment, [3, 2])])
-                segmentTemp = np.vstack([segmentTemp, np.append(segment, [4, 2])])
+                for dim in range(self.dimension):
+                    segmentTemp = np.vstack([segmentTemp, np.append(segment, [dim+1, 1])])
+                    segmentTemp = np.vstack([segmentTemp, np.append(segment, [dim+1, 2])])
 
         segmentTemp = np.delete(segmentTemp, 0, axis=0)
         i = 0
